3d477af2a378a9a5c0f6cb97e4c5b86f357ad5c1
Giovanni Fabiani <fabiani.giovanni@gmail.com>
2024-05-28 11:47:54 +0200

    Fix Exceptions

--- src/main/java/org/meross4j/comunication/AbstractHttpConnector.java
+++ src/main/java/org/meross4j/comunication/AbstractHttpConnector.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
