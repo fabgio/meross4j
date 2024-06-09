95314867580ecb9e985b8f67ce1e62caf875b690
Giovanni Fabiani <fabiani.giovanni@gmail.com>
2024-06-09 16:00:29 +0200

    update ToggleX' Namespace

--- src/main/java/org/meross4j/command/ToggleX.java
+++ src/main/java/org/meross4j/command/ToggleX.java
@@ -27,7 +27,7 @@ public class ToggleX {
         @Override
         public String create(String type) {
             String payload = """
-                        {'togglex'": {"onoff": 0, "channel": 0}}
+                        {'togglex': {"onoff": 0, "channel": 0}}
                         """;
             return MerossMqttConnector.buildMqttMessage("SET", MerossConstants.Namespace.CONTROL_TOGGLEX.getValue(), payload);
         }
