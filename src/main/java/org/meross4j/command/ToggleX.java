550aecf860b78d5f14ffa3901e19ee4a953e5dea
Giovanni Fabiani <fabiani.giovanni@gmail.com>
2024-07-19 18:34:32 +0200

    Remove Abilities from ToggleX

--- src/main/java/org/meross4j/command/ToggleX.java
+++ src/main/java/org/meross4j/command/ToggleX.java
@@ -2,7 +2,6 @@ package org.meross4j.command;
 
 import org.meross4j.comunication.MerossConstants;
 import org.meross4j.comunication.MerossMqttConnector;
-import java.util.Collections;
 import java.util.Map;
 
 public class ToggleX {
@@ -19,12 +18,6 @@ public class ToggleX {
         public byte[] createCommandType(String type) {
             Map<String,Object> payload = Map.of("togglex",Map.of("onoff",0,"channel",0));
             return MerossMqttConnector.buildMqttMessage("SET", MerossConstants.Namespace.CONTROL_TOGGLEX.getValue(),payload);
-        }
-    }
-    public static class abilities implements Command {
-        @Override
-        public byte[] createCommandType(String type) {
-            return MerossMqttConnector.buildMqttMessage("GET", MerossConstants.Namespace.SYSTEM_ABILITY.getValue(), Collections.emptyMap());
         }
     }
 }
