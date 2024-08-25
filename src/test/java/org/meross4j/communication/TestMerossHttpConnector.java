7605b1979bf409cad309ee9eb563765fd708cdb6
Giovanni Fabiani <fabiani.giovanni@gmail.com>
2024-08-25 22:06:19 +0200

    Final refactoring

--- src/test/java/org/meross4j/communication/TestMerossHttpConnector.java
+++ src/test/java/org/meross4j/communication/TestMerossHttpConnector.java
@@ -9,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Optional;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
